e31d3b584361c8695efb60934e12c28b4f7fcd32
Alexander Thurn <athurn84@gmail.com>
2025-09-24 11:19:58 +0200

    bootstrap js

---
+++ web/html/res/script.js
@@ -0,0 +1,23 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[[\]]/g, '\$&' );
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function fetchData(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('api-result').textContent = data.message;
+        });
+}
+
+const demoMode = getParameterByName('demo') === 'true';
+const apiUrl = demoMode ? '../api/index.php?fake=true' : '../api/index.php';
+
+document.getElementById('api-button').addEventListener('click', function() {
+    fetchData(apiUrl);
+});
